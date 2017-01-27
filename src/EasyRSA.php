d36aa31fde8936637138ef67d6e84ce5f6cf1552
Paragon Initiative Enterprises <security@paragonie.com>
2017-01-27 15:46:30 -0500

    Switch to KEM+DEM

--- src/EasyRSA.php
+++ src/EasyRSA.php
@@ -14,63 +14,122 @@ use \ParagonIE\EasyRSA\Exception\InvalidCiphertextException;
 class EasyRSA implements EasyRSAInterface
 {
     const SEPARATOR = '$';
-    const VERSION_TAG = "EzR1";
+    const VERSION_TAG = "EzR2";
+    const LEGACY_TAG = "EzR1";
 
     /**
-     * Encrypt a message with defuse/php-encryption, using an ephemeral key, 
-     * then encrypt the key with RSA.
-     * 
+     * KEM+DEM approach to RSA encryption.
+     *
      * @param string $plaintext
      * @param PublicKey $rsaPublicKey
-     * 
+     *
      * @return string
      */
     public static function encrypt($plaintext, PublicKey $rsaPublicKey)
     {
         // Random encryption key
-        $ephemeral = Key::createNewRandomKey();
-        
-        // Encrypt the actual message
+        $random_key = random_bytes(32);
+
+        // Use RSA to encrypt the random key
+        $rsaOut = self::rsaEncrypt($random_key, $rsaPublicKey);
+
+        // Generate a symmetric key from the RSA output and plaintext
+        $symmetricKey = hash_hmac(
+            'sha256',
+            $rsaOut,
+            $random_key,
+            true
+        );
+        $ephemeral = Key::loadFromRawBytesForTestingPurposesOnlyInsecure(
+            $symmetricKey
+        );
+
+        // Now we encrypt the actual message
         $symmetric = Base64::encode(
             Crypto::encrypt($plaintext, $ephemeral, true)
         );
-        
-        // Use RSA to encrypt the encryption key
-        $storeKey = \base64_encode(
-            self::rsaEncrypt($ephemeral->saveToAsciiSafeString(), $rsaPublicKey)
-        );
-        
+
         $packaged = \implode(self::SEPARATOR,
             array(
                 self::VERSION_TAG,
-                $storeKey,
+                Base64::encode($rsaOut),
                 $symmetric
             )
         );
-        
+
         $checksum = \substr(
             \hash('sha256', $packaged),
             0,
             16
         );
-        
+
         // Return the ciphertext
-        return $packaged . self::SEPARATOR . $checksum; 
+        return $packaged . self::SEPARATOR . $checksum;
     }
-    
+
     /**
-     * 
-     * Encrypt a message with defuse/php-encryption, using an ephemeral key, 
-     * then encrypt the key with RSA.
-     * 
+     * KEM+DEM approach to RSA encryption.
+     *
      * @param string $ciphertext
      * @param PrivateKey $rsaPrivateKey
-     * 
+     *
      * @return string
      * @throws InvalidCiphertextException
      * @throws InvalidChecksumException
      */
     public static function decrypt($ciphertext, PrivateKey $rsaPrivateKey)
+    {
+        $split = explode(self::SEPARATOR, $ciphertext);
+        if (\count($split) !== 4) {
+            throw new InvalidCiphertextException('Invalid ciphertext message');
+        }
+        if (\hash_equals($split[0], self::LEGACY_TAG)) {
+            return self::legacyDecrypt($ciphertext, $rsaPrivateKey);
+        }
+        if (!\hash_equals($split[0], self::VERSION_TAG)) {
+            throw new InvalidCiphertextException('Invalid version tag');
+        }
+        $checksum = \substr(
+            \hash('sha256', implode('$', array_slice($split, 0, 3))),
+            0,
+            16
+        );
+        if (!\hash_equals($split[3], $checksum)) {
+            throw new InvalidChecksumException('Invalid checksum');
+        }
+
+        $rsaCipher = Base64::decode($split[1]);
+        $rsaPlain = self::rsaDecrypt(
+            $rsaCipher,
+            $rsaPrivateKey
+        );
+        $symmetricKey = hash_hmac(
+            'sha256',
+            $rsaCipher,
+            $rsaPlain,
+            true
+        );
+
+        $key = Key::loadFromRawBytesForTestingPurposesOnlyInsecure($symmetricKey);
+        return Crypto::decrypt(
+            Base64::decode($split[2]),
+            $key,
+            true
+        );
+    }
+
+    /**
+     * Encrypt a message with defuse/php-encryption, using an ephemeral key,
+     * then encrypt the key with RSA.
+     *
+     * @param string $ciphertext
+     * @param PrivateKey $rsaPrivateKey
+     *
+     * @return string
+     * @throws InvalidCiphertextException
+     * @throws InvalidChecksumException
+     */
+    public static function legacyDecrypt($ciphertext, PrivateKey $rsaPrivateKey)
     {
         $split = explode(self::SEPARATOR, $ciphertext);
         if (\count($split) !== 4) {
@@ -87,23 +146,23 @@ class EasyRSA implements EasyRSAInterface
         if (!\hash_equals($split[3], $checksum)) {
             throw new InvalidChecksumException('Invalid checksum');
         }
-        
+
         $key = Key::loadFromAsciiSafeString(
             self::rsaDecrypt(
                 Base64::decode($split[1]),
                 $rsaPrivateKey
             )
         );
-        return Crypto::Decrypt(
+        return Crypto::decrypt(
             Base64::decode($split[2]),
             $key,
             true
         );
     }
-    
+
     /**
      * Sign with RSASS-PSS + MGF1+SHA256
-     * 
+     *
      * @param string $message
      * @param PrivateKey $rsaPrivateKey
      * @return string
@@ -116,14 +175,14 @@ class EasyRSA implements EasyRSAInterface
             $rsa->setSignatureMode(RSA::SIGNATURE_PSS);
             $rsa->setMGFHash('sha256');
         }
-        
+
         $rsa->loadKey($rsaPrivateKey->getKey());
         return $rsa->sign($message);
     }
-    
+
     /**
      * Verify with RSASS-PSS + MGF1+SHA256
-     * 
+     *
      * @param string $message
      * @param string $signature
      * @param PublicKey $rsaPublicKey
@@ -137,14 +196,14 @@ class EasyRSA implements EasyRSAInterface
             $rsa->setSignatureMode(RSA::SIGNATURE_PSS);
             $rsa->setMGFHash('sha256');
         }
-        
+
         $rsa->loadKey($rsaPublicKey->getKey());
         return $rsa->verify($message, $signature);
     }
-    
+
     /**
      * Decrypt with RSAES-OAEP + MGF1+SHA256
-     * 
+     *
      * @param string $plaintext
      * @param PublicKey $rsaPublicKey
      * @return string
@@ -158,14 +217,14 @@ class EasyRSA implements EasyRSAInterface
             $rsa->setEncryptionMode(RSA::ENCRYPTION_OAEP);
             $rsa->setMGFHash('sha256');
         }
-        
+
         $rsa->loadKey($rsaPublicKey->getKey());
         return $rsa->encrypt($plaintext);
     }
-    
+
     /**
      * Decrypt with RSAES-OAEP + MGF1+SHA256
-     * 
+     *
      * @param string $ciphertext
      * @param PrivateKey $rsaPrivateKey
      * @return string
@@ -179,9 +238,9 @@ class EasyRSA implements EasyRSAInterface
             $rsa->setEncryptionMode(RSA::ENCRYPTION_OAEP);
             $rsa->setMGFHash('sha256');
         }
-        
+
         $rsa->loadKey($rsaPrivateKey->getKey());
-        
+
         $return = @$rsa->decrypt($ciphertext);
         if ($return === false) {
             throw new InvalidCiphertextException('Decryption failed');
